c698ad683643b2322b8143269b15b14be2970c98
Singlerush <372958366@qq.com>
2017-03-15 16:41:57 +0800

    test

--- comingo Maven Webapp/src/main/java/com/comingo/controller/TestController.java
+++ comingo Maven Webapp/src/main/java/com/comingo/controller/TestController.java
@@ -2,6 +2,8 @@ package com.comingo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 
@@ -13,9 +15,29 @@ public class TestController extends BaseController {
 	@Resource
 	TestService testService;
 	
-	@RequestMapping("/insert.action")
-	public String insert(Test test){
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public @ResponseBody
+	String sayGET(Test test) {
 		testService.insert(test);
-		return "redirect:/success.jsp";
+		return "GET Success";
+	}
+
+	@RequestMapping(value = "/test", method = RequestMethod.POST)
+	public @ResponseBody
+	String sayPOST(Test test){
+		testService.insert(test);
+		return "POST Success";
+	}
+	
+	@RequestMapping(value = "/test", method = RequestMethod.DELETE)
+	public @ResponseBody
+	String sayDEL(Test test){
+		return "DELETE Success";
+	}
+	
+	@RequestMapping(value = "/test", method = RequestMethod.PUT)
+	public @ResponseBody
+	String sayPUT(Test test){
+		return "PUT Success";
 	}
 }	
